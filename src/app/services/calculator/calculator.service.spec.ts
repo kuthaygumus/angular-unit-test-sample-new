e2cb86d4c20ead680e43f146a784d2f4aefe7cfa
Kuthay Gumus <kuthaygumus@gmail.com>
2023-12-28 00:21:06 +0300

    beforeEach implemented

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,20 +1,22 @@
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
-  let service: CalculatorService;
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+
+  beforeEach(() => {
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
 
   it('should add two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    service = new CalculatorService(mockLoggerService);
-    let result = service.add(1, 2);
+    let result = calculator.add(1, 2);
     expect(result).toBe(3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    service = new CalculatorService(mockLoggerService);
-    let result = service.subtract(2, 1);
+    let result = calculator.subtract(2, 1);
     expect(result).toBe(1);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
