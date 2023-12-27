d2843a300865867760890baf1f491b958fa403eb
Kuthay Gumus <kuthaygumus@gmail.com>
2023-12-28 02:08:06 +0300

    test bed intro

--- src/app/component/posts/posts.component.spec.ts
+++ src/app/component/posts/posts.component.spec.ts
@@ -1,6 +1,8 @@
 import { Post } from 'src/app/models/post';
 import { PostsComponent } from './posts.component';
 import { of } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { PostService } from 'src/app/services/post/post.service';
 
 describe('Posts Component', () => {
   let POSTS: Post[];
@@ -27,15 +29,35 @@ describe('Posts Component', () => {
     ];
 
     mockPostService = jasmine.createSpyObj(['getPosts', 'deletePost']);
-    component = new PostsComponent(mockPostService);
-    mockPostService.deletePost.and.returnValue(of(true));
-    component.posts = POSTS;
+
+    TestBed.configureTestingModule({
+      providers: [
+        PostsComponent,
+        {
+          provide: PostService,
+          useValue: mockPostService,
+        },
+      ],
+    });
+
+    component = TestBed.inject(PostsComponent);
   });
 
   describe('delete', () => {
+    beforeEach(() => {
+      mockPostService.deletePost.and.returnValue(of(true));
+      component.posts = POSTS;
+    });
+
     it('should delete the selected Post from the Posts', () => {
       component.delete(POSTS[1]);
       expect(component.posts.length).toBe(2);
     })
+
+    it('should call the delete method in Post Service only once', () => {
+      component.delete(POSTS[1]);
+      expect(mockPostService.deletePost).toHaveBeenCalledTimes(1);
+    });
+
   })
 });
