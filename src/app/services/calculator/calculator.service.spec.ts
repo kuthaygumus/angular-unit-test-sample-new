84091abdf9823acff8e75d086abc00762582df3d
Kuthay Gumus <kuthaygumus@gmail.com>
2023-12-28 00:02:00 +0300

    debugger showcase

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -6,15 +6,19 @@ describe('CalculatorService', () => {
 
   it('should add two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
     service = new CalculatorService(loggerService);
     let result = service.add(1, 2);
     expect(result).toBe(3);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
     let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
     service = new CalculatorService(loggerService);
     let result = service.subtract(2, 1);
     expect(result).toBe(1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
